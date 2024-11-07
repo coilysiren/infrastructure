13bcb20a154608d0c1aca50550a62f428db9c8c1
Kai Siren <coilysiren@gmail.com>
2024-11-07 13:44:55 -0800

    full path

--- scripts/eco-server-start.sh
+++ scripts/eco-server-start.sh
@@ -4,4 +4,4 @@ set -eux
 
 eco_server_api_token=$(aws ssm get-parameter --name /eco/server-api-token --with-decryption --query Parameter.Value --output text)
 
-"$ECO_PATH/EcoServer" -userToken="$eco_server_api_token"
+/home/kai/.local/share/Steam/steamapps/common/Eco/Eco_Data/Server/EcoServer -userToken="$eco_server_api_token"
