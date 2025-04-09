8982f068c63b9e0bd73465fd568dad216445125c
Kai Siren <coilysiren@gmail.com>
2025-04-08 22:34:55 -0700

    quotes

--- scripts/backend-start.sh
+++ scripts/backend-start.sh
@@ -4,7 +4,7 @@ bsky_password=$(aws ssm get-parameter --name /bsky/password --with-decryption --
 
 env \
   PYTHONPATH=. \
-  BSKY_USERNAME=coilysiren.me \
+  BSKY_USERNAME="coilysiren.me" \
   BSKY_PASSWORD="$bsky_password" \
   /home/kai/.pyenv/shims/uvicorn \
   src.main:app --port 4000 --host 0.0.0.0
