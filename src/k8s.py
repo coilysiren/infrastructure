ff5efe3bfc0be72723bbf007602140c4dbaa6712
Kai Siren <coilysiren@gmail.com>
2025-04-12 21:13:48 -0700

    deploy_

--- src/k8s.py
+++ src/k8s.py
@@ -5,11 +5,11 @@ CERT_MANAGER_VERSION = "v1.12.16"
 
 
 @invoke.task
-def cert_manager(ctx: invoke.Context):
+def deploy_cert_manager(ctx: invoke.Context):
     ctx.run(
         f"kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/{CERT_MANAGER_VERSION}/cert-manager.yaml",
         echo=True,
     )
 
 
-k8s_collection = invoke.Collection("k8s", cert_manager)
+k8s_collection = invoke.Collection("k8s", deploy_cert_manager)
