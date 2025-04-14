82b9d0e35d401c77ca3c1f6feec30e3de5a77479
Kai Siren <coilysiren@gmail.com>
2025-04-14 10:44:59 -0700

    fixes DNS

--- src/k8s.py
+++ src/k8s.py
@@ -40,27 +40,29 @@ def cert_manager(ctx: invoke.Context):
 def dns(ctx: invoke.Context):
     ingresses = json.loads(ctx.run("kubectl get ingress --all-namespaces -o json", echo=True).stdout)
 
-    dns_challenges = {}
+    loopbacks = {}
 
     for item in ingresses["items"]:
         for ingress in item["status"]["loadBalancer"]["ingress"]:
             if ip := ingress.get("ip"):
                 for tls in item["spec"]["tls"]:
                     for host in tls["hosts"]:
-                        dns_challenges[host] = ip
-
-    print(dns_challenges)
+                        loopbacks[host] = ip
 
     with open("deploy/coredns_jinja.yml", "r", encoding="utf-8") as f:
         template = jinja2.Template(f.read())
 
     with open("deploy/coredns_filled.yml", "w", encoding="utf-8") as f:
-        f.write(template.render(dns_challenges=dns_challenges))
+        f.write(template.render(loopbacks=loopbacks))
 
-    # ctx.run("kubectl apply -f deploy/coredns_filled.yml", echo=True)
-
-    # ctx.run("kubectl apply -f deploy/coredns_root_edit.yml", echo=True)
-    # ctx.run("kubectl apply -f deploy/coredns_custom_svc.yml", echo=True)
+    ctx.run("kubectl apply -f deploy/coredns_filled.yml", echo=True)
+    ctx.run("kubectl rollout restart deployment coredns -n kube-system", echo=True)
+    ctx.run(
+        """
+        kubectl patch deployment cert-manager -n cert-manager --type=json -p='[{"op": "remove", "path": "/spec/template/spec/hostAliases"}]' --dry-run=server -o yaml
+    """,
+        echo=True,
+    )
 
 
 k8s_collection = invoke.Collection(
