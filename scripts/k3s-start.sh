cc3646ea0442de0b4c625560304b20563e35a647
Kai Siren <coilysiren@gmail.com>
2025-04-17 15:27:03 -0700

    docs

--- scripts/k3s-start.sh
+++ scripts/k3s-start.sh
@@ -6,3 +6,14 @@ TAILSCALE_IP=$(/usr/bin/tailscale ip -4 | head -n1)
   --tls-san "$TAILSCALE_IP" \
   --bind-address "$TAILSCALE_IP" \
   --advertise-address "$TAILSCALE_IP"
+
+# Activate this script with:
+#
+# sudo systemctl edit k3s
+#
+# [Service]
+# ExecStart=
+# ExecStart=bash -c /home/kai/projects/infrastructure/scripts/k3s-start.sh
+#
+# sudo systemctl daemon-reload
+# sudo systemctl restart k3s
