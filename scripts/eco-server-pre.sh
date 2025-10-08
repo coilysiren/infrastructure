1219246a7ee5720da0a7f4b217e4735b0dae792b
Kai Siren <kaisiren@navapbc.com>
2025-10-08 06:32:18 -0700

    auto updates back on

--- scripts/eco-server-pre.sh
+++ scripts/eco-server-pre.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-# /usr/games/steamcmd +force_install_dir "/home/kai/Steam/steamapps/common/EcoServer" +login balrore +app_update 739590 validate +quit
+/usr/games/steamcmd +force_install_dir "/home/kai/Steam/steamapps/common/EcoServer" +login balrore +app_update 739590 validate +quit
