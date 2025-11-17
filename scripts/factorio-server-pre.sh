2589dd5b048a7cab69bac342d63129a5eab8f627
Kai Siren <coilysiren@gmail.com>
2025-11-16 21:36:11 -0800

    factorio server

---
+++ scripts/factorio-server-pre.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+/usr/games/steamcmd +force_install_dir "/home/kai/Steam/steamapps/common/FactorioServer" +login balrore +app_update 427520 validate +quit
---
+++ scripts/factorio-server-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+/home/kai/Steam/steamapps/common/FactorioServer/bin/x64/factorio --start-server ./saves/q4-2025.zip
