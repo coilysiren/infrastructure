acaf9b012d4daed0c954d50408cb8d975126e785
Kai <firemagelynn@gmail.com>
2025-12-07 13:01:32 -0800

    Comment out skill gain multiplier in eco-server script
    Comment out the skill gain multiplier command.

--- scripts/eco-server-pre.sh
+++ scripts/eco-server-pre.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 /usr/games/steamcmd +force_install_dir "/home/kai/Steam/steamapps/common/EcoServer" +login balrore +app_update 739590 validate +quit
-inv eco.increase-skill-gain --multiplier 1.15
+# inv eco.increase-skill-gain --multiplier 1.15
