d000cda8c0ed5d710ccd25381a69ed6bec82743b
Kai <coilysiren@gmail.com>
2026-01-01 22:04:29 -0800

    Update eco-server-pre.sh

--- scripts/eco-server-pre.sh
+++ scripts/eco-server-pre.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 /usr/games/steamcmd +force_install_dir "/home/kai/Steam/steamapps/common/EcoServer" +login balrore +app_update 739590 validate +quit
-cd /home/kai/projects/infrastructure && /home/kai/.pyenv/shims/inv eco.increase-skill-gain --multiplier 1.1
+cd /home/kai/projects/infrastructure && /home/kai/.pyenv/shims/inv eco.increase-skill-gain --multiplier 1.05
